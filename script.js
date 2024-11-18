4568a53bf264d52ee97bf1c06f9c6190066cf21b
«AndreyBlinnikovv» <andrey.blinnikovv@gmail.com>
2024-11-18 20:24:42 +0300

    выполнил 3 задание

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-let num = 266219;
+'use strict'
 
-let numStr = num.toString();
-let result = 1;
+const title = prompt("Как называется ваш проект?");
+console.log(title);
 
-for (let i = 0; i < numStr.length; i++) {
-  result *= parseInt(numStr[i]);
-  }
-  result **= 3;  
-  console.log(result);
-  console.log(result.toString().substring(0, 2));
-  
+const screens = prompt("Какие типы экранов нужно разработать?");
+console.log(screens);
 
+const screenPrice = prompt("Сколько будет стоить данная работа?");
+console.log(screenPrice);
 
+const adaptive = prompt("Нужен ли адаптив на сайте?");
 
+if (adaptive == "Да" || adaptive == "да") {
+  console.log(true);
+} else {
+  console.log(false);
+}
 
-        
+const serviceQuestion1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = +prompt("Сколько это будет стоить?")
 
+const serviceQuestion2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = +prompt("Сколько это будет стоить?")
+
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+const rollback = 3;
+const servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+console.log(Math.ceil(servicePercentPrice));
+
+switch(true) {
+  case fullPrice >= 30000:
+    console.log("Даем скидку в 10%");
+    break;
+  case fullPrice >= 15000 && fullPrice < 30000:
+    console.log("Даем скидку в 5%");
+    break;  
+  case fullPrice < 15000 && fullPrice >= 0:
+    console.log("Скидка не предусмотрена");
+    break; 
+  case fullPrice < 0:
+    console.log("Что то пошло не так");
+    break;     
+}
 
 
