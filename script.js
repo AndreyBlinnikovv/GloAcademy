cdc6f289950d79cc385943cc959f09148f2d1cf6
«AndreyBlinnikovv» <andrey.blinnikovv@gmail.com>
2024-11-20 16:55:36 +0300

    выполнил задание 4

--- script.js
+++ script.js
@@ -1,41 +1,63 @@
 'use strict'
 
-const title = prompt("Как называется ваш проект?");
-console.log(title);
-
+let title = prompt("Как называется ваш проект?");
 const screens = prompt("Какие типы экранов нужно разработать?");
-console.log(screens);
-
 const screenPrice = +prompt("Сколько будет стоить данная работа?");
-console.log(screenPrice);
-
 const adaptive = confirm("Нужен ли адаптив на сайте?");
-
 const serviceQuestion1 = prompt("Какой дополнительный тип услуги нужен?");
 const servicePrice1 = +prompt("Сколько это будет стоить?")
-
 const serviceQuestion2 = prompt("Какой дополнительный тип услуги нужен?");
 const servicePrice2 = +prompt("Сколько это будет стоить?")
-
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
 const rollback = 3;
-const servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
-console.log(servicePercentPrice);
+let fullPrice;
+let allServicePrices;
+let servicePercentPrice;
 
-switch(true) {
-  case fullPrice >= 30000:
-    console.log("Даем скидку в 10%");
-    break;
-  case fullPrice >= 15000 && fullPrice < 30000:
-    console.log("Даем скидку в 5%");
-    break;  
-  case fullPrice < 15000 && fullPrice >= 0:
-    console.log("Скидка не предусмотрена");
-    break; 
-  case fullPrice < 0:
-    console.log("Что то пошло не так");
-    break;     
+
+let getAllServicePrices = function () {
+  return servicePrice1 + servicePrice2;
+};
+
+function getFullPrice() {
+  return screenPrice + allServicePrices
+}
+
+let getTitle = function() {
+  return title.trim()[0].toUpperCase() + title.trim().toLocaleLowerCase();
+};
+
+function getServicePercentPrices() {
+  return Math.ceil(fullPrice - fullPrice * (rollback / 100))
+}
+
+let showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
+};
+
+let getRollBackMassage = function () {
+  if (fullPrice >= 30000) {
+      return "Даем скидку в 10%";
+  } else if (fullPrice >= 15000 && fullPrice < 30000) {
+      return "Даем скидку в 5%";
+  } else if (fullPrice < 15000 && fullPrice >= 0) {
+      return "Скидка не предусмотрена";
+  } else {
+      return "Что-то пошло не так";
+  }
 }
 
 
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
+title = getTitle();
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(screens);
+console.log(fullPrice);
+console.log(allServicePrices);
+console.log(servicePercentPrice);
+console.log(getRollBackMassage(fullPrice));
